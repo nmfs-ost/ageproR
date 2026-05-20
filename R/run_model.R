0ece79fd25b308d06e6d52ed5ffd137f3e2f0ef3
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2026-05-19 15:50:56 -1000

    Clean up Roxygen documentaion, remove verbose

--- R/run_model.R
+++ R/run_model.R
@@ -1,40 +1,44 @@
-#' Run an AGEPRO Input File
+#' Run the AGEPRO Cacluation Engine Binary with a AGEPRO Input File
 #'
-#' Function
+#' Wrapper function to call the Binary
 #'
 #' @param exepath Path of the Agepro Calcuation engine
 #' @param agepro_args Typically used for AGEPRO Input Files.
 #' @param outdir Output path
 #' @param save_logfile Option to save AGEPRO Calcuation Engine ouput to logfile
 #' @param fn_logfile Logfile Filename
-#' @param verbose Option to show verbose output
-#' @template elipses
 #'
 #' @export
 #' @examples
 #' \dontrun{
 #'
-#' wdir <- here::here()
-#' inpfile <- file.path(wdir,)
-#' ageproR::run_model(wdir,"AGEPRO.EXE",)
+#' inp_file <- file.path(find.package("ageproR"),"Example_UKU.INP")
+#'
+#' # For this example, save the Agepro Calcuation Engine Binary in
+#' # the 'agepro' subdirectory of home directory 'R_USER'
+#' exepath <- path.expand("~/agepro/AGEPRO.EXE")
+#' if(.Platform$OS.type == "windows") {
+#'   exepath <- normalizePath(exepath, winslash="/")
 #' }
+#'
+#' ageproR::run_model(exepath, inp_file, tempdir())
+#' }
+#'
 run_model <- function(
   exepath = "agepro.exe",
   agepro_args = "",
   outdir = getwd(),
   save_logfile = FALSE,
-  fn_logfile = tempfile(format(Sys.time(), "%Y%m%d_%H%M_"), fileext = ".txt"),
-  verbose = TRUE,
-  ...
+  fn_logfile = tempfile(format(Sys.time(), "%Y%m%d_%H%M_"), fileext = ".txt")
 ) {
+  # Validate exepath
+  checkmate::assert_character(exepath, len = 1)
+  validate_calc_engine_binary(exepath)
+
   # Valadate outdir
   if (isFALSE(checkmate::test_directory_exists(outdir))) {
     stop("Invalid AGEPRO Output Directory")
   }
-
-  # Validate exepath
-  checkmate::assert_character(exepath, len = 1)
-  validate_calc_engine_binary(exepath, verbose)
 
   cout <- tryCatch(
     {
@@ -42,7 +46,7 @@ run_model <- function(
       system2(
         command = exepath,
         args = agepro_args,
-        stdout = ifelse(save_logfile, "", TRUE),
+        stdout = ifelse(save_logfile, TRUE, ""),
         stderr = ""
       )
     },
@@ -55,12 +59,14 @@ run_model <- function(
   )
 
   if (isTRUE(save_logfile)) {
-    write_logfile(cout, file.path(outdir, fn_logfile), verbose)
+    write_logfile(cout, file.path(outdir, fn_logfile))
   }
+
+  return(cout)
 }
 
 
-write_logfile <- function(cout, fn_logfile, verbose = TRUE) {
+write_logfile <- function(cout, fn_logfile) {
   #TODO: Verify type of cout
 
   writeLines(
@@ -74,20 +80,15 @@ write_logfile <- function(cout, fn_logfile, verbose = TRUE) {
     ),
     cout
   )
-  if (verbose) {
-    cli::cli_alert_info("Written to ")
-  }
 }
 
 
 #' Validates file path for AGEPRO Calcuation Engine Binary
 #'
 #' @param exepath AGEPRO Calcuuation Path Binary
-#' @param verbose Logical option to output to RConsole.
 #'
 validate_calc_engine_binary <- function(
-  exepath = file.path(getwd(), "agepro.exe"),
-  verbose = FALSE
+  exepath = file.path(getwd(), "agepro.exe")
 ) {
   #Validate exepath: The path
   if (isFALSE(checkmate::test_file_exists(exepath, extension = "exe"))) {
