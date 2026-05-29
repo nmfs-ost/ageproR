3bf75d810a4b3ab2131f22f64d02248ee511263d
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2026-05-29 11:53:22 -1000

    write_inp: remove option_flag references  causing errors

--- R/agepro_model.R
+++ R/agepro_model.R
@@ -1236,16 +1236,16 @@ agepro_inp_model <- R6Class(
               self$rebuild$get_inp_lines(delimiter)
             },
             self$options$get_inp_lines(delimiter),
-            if (self$retroadjust$flag$op$enable_retrospective_adjustments) {
+            if (self$retroadjust$enable_retrospective_adjustments) {
               self$retroadjust$get_inp_lines(delimiter)
             },
-            if (self$bounds$flag$op$enable_max_bounds) {
+            if (self$bounds$enable_max_bounds) {
               self$bounds$get_inp_lines(delimiter)
             },
-            if (self$scale$flag$op$enable_scaling_factors) {
+            if (self$scale$enable_scaling_factors) {
               self$scale$get_inp_lines(delimiter)
             },
-            if (self$perc$flag$op$enable_percentile_summary) {
+            if (self$perc$enable_percentile_summary) {
               self$perc$get_inp_lines(delimiter)
             }
           )
