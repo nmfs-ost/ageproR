99f78f5147d6cbb650f0eddf26e1ae374cc92c5f
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2026-07-27 14:47:02 -1000

    Fixup testthat scripts to use `system.file` and use package example files. Fixes #126

--- tests/testthat/test-agepro_model.R
+++ tests/testthat/test-agepro_model.R
@@ -13,38 +13,67 @@ test_that("multiplication works", {
 # - num_recruits = 1
 # - seed = 300
 test_that("New agepro_model (Year: 2019-2026, Age: 1-32, num_pop_sims: 1000, num_fleets: 4, 1 NULL Recruitment, discards: 0, seed: 300), and NULL Bootstrap file", {
-  expect_snapshot(ageproR::agepro_model$new(2019,2026,1,32,1000,4,1,0,300), cnd_class = TRUE)
+  expect_snapshot(
+    ageproR::agepro_model$new(2019, 2026, 1, 32, 1000, 4, 1, 0, 300),
+    cnd_class = TRUE
+  )
 })
 
 
-#Note: The bootstrap_file output is masked to a fixed value to fix varying outputs using testthat and running tests
-#with devtools::check()
 test_that("example/Example1.INP exists", {
-  inpfile_path <- file.path(testthat::test_path(),"example","Example1.INP")
+  inpfile_path <- system.file("example", "Example1.INP", package = "ageproR")
   expect_true(file.exists(inpfile_path))
 })
 test_that("example/Example1.BSN exists", {
-  bsnfile_path <- file.path(testthat::test_path(),"example","Example1.BSN")
+  bsnfile_path <- system.file("example", "Example1.BSN", package = "ageproR")
   expect_true(file.exists(bsnfile_path))
 })
 
 
-#Opening Agepro Model with Example1.INP works
-#TODO: Mockup file dialog workflow
+# Opening Agepro Model with Example1.INP works
+# Note: Using original agepro_inp_model$read_inp workflow.
 test_that("Opening example/Example1.INP is imported to test agepro_inp_model", {
-  expect_snapshot(test <- ageproR::agepro_inp_model$new(seed=300), cnd_class = TRUE)
-  expect_snapshot(test$read_inp(file.path(testthat::test_path(),"example/Example1.inp")), cnd_class = TRUE,
-                  transform = \(lines) gsub(test$bootstrap$bootstrap_file, "<path>", lines, fixed = TRUE))
+  # Create agepro_inp_model "test"
+  expect_snapshot(
+    test <- ageproR::agepro_inp_model$new(seed = 300),
+    cnd_class = TRUE
+  )
+  # Create a input file and check if path works
+  inpfile_path <- system.file("example", "Example1.INP", package = "ageproR")
+  expect_true(file.exists(inpfile_path))
+  # import AGEPRO input file data to "test"
+  ## Note: The bootstrap_file output is masked to a fixed value to fix varying outputs
+  ## using testthat and running tests with devtools::check()
+  expect_snapshot(
+    test$read_inp(inpfile_path),
+    cnd_class = TRUE,
+    transform = \(lines) {
+      gsub(test$bootstrap$bootstrap_file, "<path>", lines, fixed = TRUE)
+    }
+  )
 })
 
 #Test that importing agepro_inp_model data from example/Example1.INP (with bootstrap_file)
 #is exported to a new to agepro_json_model class instance.
-test_that("Import 'test' agepro_inp_model class data to 'json_test' agepro_json_model class",{
-  expect_snapshot(test <- ageproR::agepro_inp_model$new(seed=300), cnd_class = TRUE)
-  expect_snapshot(test$read_inp(file.path(testthat::test_path(),"example/Example1.INP")), cnd_class = TRUE,
-                  transform = \(lines) gsub(test$bootstrap$bootstrap_file, "<path>", lines, fixed = TRUE))
-  expect_snapshot(test_json <- ageproR::agepro_json_model$new(0,9,0,1,1000,4,1,0,300), cnd_class = TRUE)
-  expect_snapshot(test_json$import_agepro_inp_model(test), cnd_class = TRUE)
+test_that("Import agepro_inp_model (test_inp) class data to agepro_json_model (test_json) class", {
+  # Create a input file and check if path works
+  inpfile_path <- system.file("example", "Example1.INP", package = "ageproR")
+  expect_true(file.exists(inpfile_path))
+  # import_agepro_model from input file to "test_inp"
+  # Note: The bootstrap_file output is masked to a fixed value to fix varying
+  # outputs using testthat and running tests with devtools::check()
+  expect_snapshot(
+    test_inp <- import_agepro_model(inpfile_path),
+    cnd_class = TRUE,
+    transform = \(lines) {
+      gsub(test_inp$bootstrap$bootstrap_file, "<path>", lines, fixed = TRUE)
+    }
+  )
+  # Create a agepro_json_model as "test_json"
+  expect_snapshot(
+    test_json <- ageproR::agepro_json_model$new(0, 9, 0, 1, 1000, 4, 1, 0, 300),
+    cnd_class = TRUE
+  )
+  # Set "test_json" wtih "test_inp" values
+  expect_snapshot(test_json$import_agepro_inp_model(test_inp), cnd_class = TRUE)
 })
-
-
